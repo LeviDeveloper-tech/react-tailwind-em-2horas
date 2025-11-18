705cf3bd20901100cf06a1259e61a72015de0b8d
Levi-DevTech <levi.devtech@gmail.com>
2025-11-18 00:08:40 -0300

    Adicionar funcionalidade de exclusão de tarefas ao gerenciador de tarefas
    Introduz um manipulador de eventos `onDeleteTaskClick` em `App.jsx` e o passa para `Tasks.jsx`. Adiciona um botão de exclusão a cada item da lista de tarefas, permitindo que os usuários removam tarefas da lista.

--- src/App.jsx
+++ src/App.jsx
@@ -37,13 +37,19 @@ function App() {
     })
     setTasks(newTasks)
   }
+  
+  function onDeleteTaskClick(taskID){
+    const newTasks = tasks.filter(task => task.id !== taskID)
+    setTasks(newTasks)
+  }
+
 
   return (
 
     <div className='w-screen h-screen bg-slate-600 flex justify-center p-6'>
       <div className='w-[500px]'>
         <h1 className='text-3xl text-slate-100 font-bold text-center'>Gerenciador de tarefas</h1>
-        <Tasks tasks={tasks} onTaskClick={onTaskClick}/>
+        <Tasks tasks={tasks} onDeleteTaskClick={onDeleteTaskClick} onTaskClick={onTaskClick}/>
       </div>
     </div>
   )
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,21 +1,27 @@
-import { ChevronRight } from "lucide-react";
+import { ChevronRight, Trash2Icon, TrashIcon } from "lucide-react";
 
 function Tasks(props){
     return(
        
         <div className="p-5">
-            <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow  ">{props.tasks.map(function (tasks){ 
+            <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow ">{props.tasks.map(function (tasks){ 
                 return (
                 <li key={tasks.id} className="flex gap-2
                 ">
-                    <button onClick={() => props.onTaskClick(tasks.id)} className="bg-slate-400 w-full text-left text-white p-2 rounded-md hover:cursor-pointer">
+                    {/* Task title */}
+                    <button onClick={() => props.onTaskClick(tasks.id)} className={`bg-slate-400 w-full text-left font-semibold text-slate-900 p-2 rounded-md hover:cursor-pointer hover:bg-slate-500 hover:text-slate-50 transition-colors 
+                        ${tasks.isCompleted && 'line-through'}`}>
                         {tasks.title}
-                    {tasks.isCompleted ? " COMPLETE" : " INCOMPLETED"}
                     </button>
-                    <button className="bg-slate-400 p-2 rounded-md text-white">
+                    {/* Botão de detalhes */}
+                    <button className="bg-slate-400 p-2 rounded-md text-slate-900 hover:cursor-pointer hover:bg-slate-500 hover:text-slate-50 transition-colors">
                         <ChevronRight/>
                     </button>
-                    </li> 
+                    {/* Botão de deletar */}
+                    <button onClick={() => props.onDeleteTaskClick(tasks.id)} className="bg-slate-400 p-2 rounded-md text-slate-900 hover:cursor-pointer hover:bg-slate-500 hover:text-slate-50 transition-colors">
+                        <TrashIcon/>
+                    </button>
+                </li> 
                 )
         })}</ul> 
         </div>
